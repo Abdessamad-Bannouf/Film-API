043c8783f8619a9a499703b3274498b1b0c265cc
Abdessamad Bannouf <abdessamad.bannouf@laposte.net>
2023-12-05 22:47:57 +0100

    Add the list categories and single category methods for the APIs

--- backend/src/Entity/Categorie.php
+++ backend/src/Entity/Categorie.php
@@ -6,17 +6,35 @@ use App\Repository\CategorieRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Hateoas\Configuration\Annotation as Hateoas;
 use JMS\Serializer\Annotation as Serializer;
 
 /**
  * @Serializer\ExclusionPolicy("ALL")
+ * @Hateoas\Relation(
+ *     name = "self",
+ *     href = @Hateoas\Route(
+ *         "app_api_categorie_single",
+ *         parameters = { "id" = "expr(object.getId())" },
+ *         absolute = true
+ *     ),
+ *     exclusion = @Hateoas\Exclusion(groups={"categorie:single", "categorie:list"})
+ * )
+ * 
+ * @Hateoas\Relation(
+ *     name = "categories",
+ *     embedded = @Hateoas\Embedded(
+ *         "expr(object.getFilms())",
+ *         exclusion = @Hateoas\Exclusion(groups={"categorie:single", "categorie:list"})
+ *     )
+ * )
  */
 #[ORM\Entity(repositoryClass: CategorieRepository::class)]
 class Categorie
 {
     /**
      * @Serializer\Expose
-     * @Serializer\Groups({"film:list", "film:single", "film:patch", "film:put"})
+     * @Serializer\Groups({"film:list", "film:single", "film:patch", "film:put", "categorie:single", "categorie:list"})
      */
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -25,11 +43,15 @@ class Categorie
 
     /**
      * @Serializer\Expose
-     * @Serializer\Groups({"film:list", "film:single", "film:patch", "film:put"})
+     * @Serializer\Groups({"film:list", "film:single", "film:patch", "film:put", "categorie:single", "categorie:list"})
      */
     #[ORM\Column(length: 255)]
     private ?string $nom = null;
 
+    /**
+     * @Serializer\Expose
+     * @Serializer\Groups({"categorie:single"})
+     */
     #[ORM\ManyToMany(targetEntity: Film::class, inversedBy: 'categories')]
     private Collection $films;
 
